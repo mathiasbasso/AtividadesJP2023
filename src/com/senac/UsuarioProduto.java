6e0817f8118c2b40c2a21d07bcba474e4b1384b4
Mathias Basso <mathias201711@gmail.com>
2023-07-03 21:02:46 -0300

    commit atividades : 03/07

--- src/com/senac/UsuarioProduto.java
+++ src/com/senac/UsuarioProduto.java
@@ -1,25 +1,32 @@
 package com.senac;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class UsuarioProduto {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int preco1, preco2, preco3;
-
+        double preco1, preco2, preco3;
         String resposta1, resposta2, resposta3;
+//        String nome = JOptionPane.showInputDialog("Qual é o seu nome: ");
+//        JOptionPane.showMessageDialog(null, "Olá " + nome);
+
         System.out.println("Entre com o nome do primeiro produto: \n");
         resposta1 = entrada.next();
         System.out.println("Qual o valor deste primeiro produto?: \n ");
-        preco1 = entrada.nextInt();
+        preco1 = entrada.nextDouble();
         System.out.println("Entre com o nome do segundo produto: \n");
         resposta2 = entrada.next();
         System.out.println("Qual o valor deste segundo produto?: \n ");
-        preco2 = entrada.nextInt();
+        preco2 = entrada.nextDouble();
         System.out.println("Entre com o nome do terceiro produto: \n");
         resposta3 = entrada.next();
         System.out.println("Qual o valor deste terceiro produto?: \n ");
-        preco3 = entrada.nextInt();
+        preco3 = entrada.nextDouble();
+
+        System.out.println("----------------------------------");
+        System.out.println("ANALISE O PRODUTO MAIS BARATO AQUI");
+        System.out.println("----------------------------------");
 
         if (preco1 < preco2 &&  preco1 < preco3){
             System.out.println("o produto "+ resposta1+ "é o mais barato, custa apenas "+preco1+ " R$");
