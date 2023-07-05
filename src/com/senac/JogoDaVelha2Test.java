a8dc76057f33bcf52c8c0d468c365b596aa285ac
Mathias Basso <mathias201711@gmail.com>
2023-07-05 20:37:53 -0300

    commit atividades : 05/07

---
+++ src/com/senac/JogoDaVelha2Test.java
@@ -0,0 +1,14 @@
+package com.senac;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JogoDaVelha2Test {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkWinner() {
+    }
+}
