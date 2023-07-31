6de122edb00137a2ac875e5d192300f2a26d86dc
Mathias Basso <mathias201711@gmail.com>
2023-07-31 20:15:48 -0300

    commit atividades : 31/07

---
+++ src/bugs/DivideByZero.java
@@ -0,0 +1,10 @@
+package bugs;
+
+public class DivideByZero {
+    public static void main(String[] args) {
+        int number = 10;
+        int divisor = 1;
+        int result = number / divisor;
+        System.out.println(result);
+    }
+}
