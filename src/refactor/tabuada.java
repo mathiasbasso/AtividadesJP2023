dbf85b90d3df9179a0d62da899a7da7f2cc55429
Mathias Basso <mathias201711@gmail.com>
2023-07-31 21:47:46 -0300

    commit atividades : 31/07

--- src/refactor/tabuada.java
+++ src/refactor/tabuada.java
@@ -1,8 +1,13 @@
 package refactor;
 
+import java.util.Scanner;
+
 public class tabuada {
     public static void main(String[] args) {
-        int numero = 5;
+        Scanner escolha = new Scanner(System.in);
+        int numero ;
+        System.out.println("Escolha um numero  para ver seu resultado quando multiplicado de 1 até 10: ");
+        numero = escolha.nextInt();
         for (int i = 1; i <= 10; i++) {
             System.out.println(numero + " x " + i + " = " + numero * i);
         }
